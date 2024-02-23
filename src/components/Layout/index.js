cd82d7cbd0fd635be8e37acde1854317541d29ae
hatran216004 <qiqisokiu.321@gmail.com>
2024-02-24 00:16:41 +0700

    Cấu hình Router/Layout cho dự án

---
+++ src/components/Layout/index.js
@@ -0,0 +1,6 @@
+// Import `DefaultLayout` từ tệp `DefaultLayout.js`
+import DefaultLayout from './DefaultLayout';
+import HeaderOnly from './HeaderOnly';
+
+// Xuất `DefaultLayout` như là một phần của mô-đun hiện tại
+export { DefaultLayout, HeaderOnly };
